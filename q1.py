e9afc9d6884aabb50a1e3fa4d4ad6f38daa62e8e
marinakontalexi <kontalexim@gmail.com>
2023-02-05 14:17:23 +0200

    q1

--- q1.py
+++ q1.py
@@ -2,6 +2,8 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql import SparkSession
 import os, sys, time
+import shutil
+import glob
 
 #create spark session
 spark = SparkSession.builder.master("spark://192.168.0.1:7077").getOrCreate()
@@ -13,17 +15,26 @@ print("Spark Session Started")
 
 #initialize dataset
 q1 = spark.read.option("header", "true").option("inferSchema", "true").parquet(hdfs_path + "yellow_tripdata_2022-prwtoi6.parquet")
-#q1=spark.read.parquet("./data/yellow_tripdata_2022-prwtoi6.parquet")
+zone_lookups_df = spark.read.option("header", "true").option("inferSchema", "true").csv(hdfs_path + "taxi+_zone_lookup.csv")
 
 #add column for month
 q1new = q1.withColumn("month", month("tpep_pickup_datetime"))
 
 #sql query
 q1new.createOrReplaceTempView("data")
-query1 = spark.sql("select * from data where month == 3 and DOLocationID == 12 order by Tip_amount desc limit 1")
+zone_lookups_df.createOrReplaceTempView("zone_lookups")
+query1 = spark.sql("""
+SELECT data.* 
+FROM data, zone_lookups 
+WHERE data.month == 3 and zone_lookups.Zone == "Battery Park" and data.DOLocationID == zone_lookups.LocationID
+ORDER BY Tip_amount desc limit 1""")
 
 start = time.time()
-query1.show()
+#query1.show()
+query1.write.option("header","true").option("delimiter",",").csv("q1_result")
 time_elapsed = time.time() - start
 print("Time elapsed: ", time_elapsed) 
 spark.stop()
+files = glob.glob("./q1_result/*.csv")
+shutil.move(files[0], "q1_resulv.csv")
+shutil.rmtree("./q1_result")
